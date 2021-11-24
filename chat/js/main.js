ce958fec42dc7ff2b2b457bd4eab49c0c633ba3e
cookie0215 <jina_0215@naver.com>
2021-11-25 00:38:26 +0900

    feat: Output a value into Console window
    I entered a value of input
    And, output the value I entered into the console window

--- chat/js/main.js
+++ chat/js/main.js
@@ -1,8 +1,10 @@
+const dogTalk = document.getElementById("dogTalk");
 const myTalk = document.getElementById("myTalk");
 const chatBtn = document.getElementById("chatBtn");
 
 chatBtn.addEventListener('click', e => {
     e.preventDefault();
 
-    console.log("말하기");
+    let talk = myTalk.value = "멍군아 안녕!";
+    console.log(talk);
 });
