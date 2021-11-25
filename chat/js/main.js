d7a07b8c0cb88e5463f645a2eca20b0d84008c02
cookie0215 <jina_0215@naver.com>
2021-11-25 23:44:38 +0900

    feat: 조건에 따라 이미지 변화 및 Math.random 사용
    - 조건문에 따라 이미지 변화 및 텍스트 변경
    - random객체를 이용해 한가지 물음에 상반된 두가지 반응이 랜덤하게 나올
      수 있도록 만듬

--- chat/js/main.js
+++ chat/js/main.js
@@ -1,10 +1,27 @@
-const dogTalk = document.getElementById("dogTalk");
-const myTalk = document.getElementById("myTalk");
+let dogTalk = document.getElementById("dogTalk");
+let myTalk = document.getElementById("myTalk");
+let img = document.querySelector(".dogImg img");
 const chatBtn = document.getElementById("chatBtn");
 
 chatBtn.addEventListener('click', e => {
     e.preventDefault();
-
-    let talk = myTalk.value = "멍군아 안녕!";
-    console.log(talk);
+    activation();
 });
+
+function activation(){
+    if(myTalk.value === "멍군아 안녕!") {
+        dogTalk.innerText = "안녕이다멍!";
+        img.setAttribute("src", `img/dog2.png`);
+    }else if(myTalk.value === "산책갈까?"){
+        let num = Math.floor(Math.random()*2);
+        if(num === 1){
+            img.setAttribute("src", `img/dog3.png`);
+            dogTalk.innerText = "좋다멍! 빨리 나가자멍!";
+        }else {
+            img.setAttribute("src", `img/dog4.png`);
+            dogTalk.innerText = "귀찮다멍...저리가라 주인!";
+        }
+    }
+    
+    myTalk.value = "";
+}
